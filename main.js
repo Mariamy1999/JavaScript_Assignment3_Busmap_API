42d86e92e187712741fbce16c0151f43554df19b
Mariana Odam Noda <W0521335@nscc.ca>
2026-03-06 13:48:38 -0300

    Second commit. Still need to figure out the filter, but the bus system is working

--- main.js
+++ main.js
@@ -1,70 +1,134 @@
 // IIFE
-(() => {
+(function(){
 
-function loadBusData(){
+const API_URL = "https://halifax-transit-data.onrender.com/vehicles";
+const REFRESH_INTERVAL = 7000;
 
-  fetch("https://halifax-transit-data.onrender.com/vehicles")
-  .then(res => res.json())
-  .then(data => {
-      console.log(data);
-  });
+let map;
+let busLayer = null;
 
-  const filteredBuses = data.entity.filter(bus => {
-    const route = bus.vehicle.trip.routeId;
-    return route >= 1 && route <= 10;
-  });
-
-  const geoJSON = {
-    type: "FeatureCollection",
-    features: filteredBuses.map(bus => ({
-        type: "Feature",
-        properties: {
-            route: bus.vehicle.trip.routeId
-        },
-        geometry: {
-            type: "Point",
-            coordinates: [
-                bus.vehicle.position.longitude,
-                bus.vehicle.position.latitude
-            ]
-        }
-    }))
-  };
-
-  L.geoJSON(geoJSON).addTo(map);
-
-  const busIcon = L.icon({
+const busIcon = L.icon({
     iconUrl: "bus.png",
-    iconSize: [30, 30]
-  });
+    iconSize: [30,30],
+    iconAnchor: [15,15]
+});
 
-  L.geoJSON(geoJSON, {
-    pointToLayer: function(feature, latlng) {
-        return L.marker(latlng, {icon: busIcon});
-    }
-  }).addTo(map);
 
-  leaflet-rotatedmarker.js
+function createMap(){
 
-  return L.marker(latlng, {
-    icon: busIcon,
-    rotationAngle: feature.properties.bearing
-  });
+    map = L.map("map").setView([44.6488, -63.5752], 13);
 
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup(
-        "Route: " + feature.properties.route
-    );
-  }
-
-  setInterval(() => {
-    loadBusData();
-  }, 7000);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+        attribution:"© OpenStreetMap"
+    }).addTo(map);
 
 }
 
-loadBusData();
+
+function fetchTransitData(){
+
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+
+        console.log("RAW DATA:",data);
+
+        const filtered = filterRoutes(data);
+
+        const geojson = convertToGeoJSON(filtered);
+
+        console.log("GEOJSON:",geojson);
+
+        plotVehicles(geojson);
+
+    });
+
+}
 
 
+function filterRoutes(data){
 
-})()
+    return data.entity.filter(bus => {
+
+        const route = bus.vehicle.trip.routeId;
+
+        return route >=1 && route <=10;
+
+    });
+
+}
+
+
+function convertToGeoJSON(buses){
+
+    return {
+        type:"FeatureCollection",
+
+        features: buses.map(bus => ({
+
+            type:"Feature",
+
+            properties:{
+                route: bus.vehicle.trip.routeId,
+                bearing: bus.vehicle.position.bearing
+            },
+
+            geometry:{
+                type:"Point",
+
+                coordinates:[
+                    bus.vehicle.position.longitude,
+                    bus.vehicle.position.latitude
+                ]
+            }
+
+        }))
+    };
+
+}
+
+
+function plotVehicles(geojson){
+
+    if(busLayer){
+        map.removeLayer(busLayer);
+    }
+
+    busLayer = L.geoJSON(geojson,{
+
+        pointToLayer:function(feature,latlng){
+
+            return L.marker(latlng,{
+                icon:busIcon,
+                rotationAngle:feature.properties.bearing
+            });
+
+        },
+
+        onEachFeature:function(feature,layer){
+
+            layer.bindPopup(
+                "Route: " + feature.properties.route
+            );
+
+        }
+
+    }).addTo(map);
+
+}
+
+
+function startApp(){
+
+    createMap();
+
+    fetchTransitData();
+
+    setInterval(fetchTransitData, REFRESH_INTERVAL);
+
+}
+
+
+startApp();
+
+})();
